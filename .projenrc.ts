31ba7d6cea95c5de02430e5016850fe1cf64031a
Niko Virtala <niko.virtala@hey.com>
2024-11-02 21:30:33 +0200

    chore: try wider indentation

--- .projenrc.ts
+++ .projenrc.ts
@@ -2,38 +2,50 @@ import { AwsCdkApp } from "@nikovirtala/projen-aws-cdk-app";
 import { javascript } from "projen";
 
 const project = new AwsCdkApp({
-  authorEmail: "niko.virtala@hey.com",
-  authorName: "Niko Virtala",
-  autoApproveOptions: {
-    secret: "GITHUB_TOKEN",
-    allowedUsernames: ["nikovirtala"],
-  },
-  cdkVersion: "2.165.0",
-  defaultReleaseBranch: "main",
-  dependabot: false,
-  depsUpgradeOptions: {
-    workflowOptions: {
-      labels: ["auto-approve", "auto-merge"],
+    authorEmail: "niko.virtala@hey.com",
+    authorName: "Niko Virtala",
+    autoApproveOptions: {
+        secret: "GITHUB_TOKEN",
+        allowedUsernames: ["nikovirtala"],
     },
-  },
-  description: "Example of using LLRT (Low Latency Runtime) on AWS Lambda.",
-  devDeps: ["@nikovirtala/projen-aws-cdk-app"],
-  license: "MIT",
-  licensed: true,
-  mergify: true,
-  name: "cdk-lambda-llrt-demo",
-  packageManager: javascript.NodePackageManager.PNPM,
-  pnpmVersion: "9",
-  prettier: true,
-  prettierOptions: {
-    settings: {
-      printWidth: 120,
-      tabWidth: 2,
-      trailingComma: javascript.TrailingComma.ALL,
+    cdkVersion: "2.165.0",
+    defaultReleaseBranch: "main",
+    dependabot: false,
+    depsUpgradeOptions: {
+        workflowOptions: {
+            labels: ["auto-approve", "auto-merge"],
+        },
     },
-  },
-  projenrcTs: true,
-  repository: "https://github.com/nikovirtala/cdk-lambda-llrt-demo.git",
+    description: "Example of using LLRT (Low Latency Runtime) on AWS Lambda.",
+    devDeps: ["@nikovirtala/projen-aws-cdk-app"],
+    license: "MIT",
+    licensed: true,
+    mergify: true,
+    name: "cdk-lambda-llrt-demo",
+    packageManager: javascript.NodePackageManager.PNPM,
+    pnpmVersion: "9",
+    prettier: true,
+    prettierOptions: {
+        settings: {
+            printWidth: 120,
+            tabWidth: 4,
+            trailingComma: javascript.TrailingComma.ALL,
+        },
+    },
+    projenrcTs: true,
+    repository: "https://github.com/nikovirtala/cdk-lambda-llrt-demo.git",
+});
+
+project.vscode?.extensions.addRecommendations("dbaeumer.vscode-eslint", "esbenp.prettier-vscode");
+
+project.vscode?.settings.addSettings({
+    "editor.codeActionsOnSave": {
+        "source.fixAll": "always",
+        "source.organizeImports": "always",
+    },
+    "editor.defaultFormatter": "esbenp.prettier-vscode",
+    "editor.formatOnSave": true,
+    "editor.tabSize": 4,
 });
 
 project.synth();
