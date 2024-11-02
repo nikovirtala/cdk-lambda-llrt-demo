5bdc3f5ca18452626ce7019be67df4a58ea65e6a
Niko Virtala <niko.virtala@hey.com>
2024-11-02 21:31:49 +0200

    refactor: move stack to separate file

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,15 @@
-import { App, Stack, StackProps } from "aws-cdk-lib";
-import { Construct } from "constructs";
-
-export class MyStack extends Stack {
-  constructor(scope: Construct, id: string, props: StackProps = {}) {
-    super(scope, id, props);
-
-    // define resources here...
-  }
-}
+import { App } from "aws-cdk-lib";
+import { LambdaLlrtStack } from "./stacks.ts";
 
 // for development, use account/region from cdk cli
 const devEnv = {
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
 };
 
 const app = new App();
 
-new MyStack(app, "cdk-lambda-llrt-demo-dev", { env: devEnv });
-// new MyStack(app, 'cdk-lambda-llrt-demo-prod', { env: prodEnv });
+new LambdaLlrtStack(app, "cdk-lambda-llrt-demo-dev", { env: devEnv });
+// new LambdaLlrtStack(app, 'cdk-lambda-llrt-demo-prod', { env: prodEnv });
 
 app.synth();
--- test/main.test.ts
+++ test/main.test.ts
@@ -1,11 +1,11 @@
 import { App } from "aws-cdk-lib";
 import { Template } from "aws-cdk-lib/assertions";
-import { MyStack } from "../src/main";
+import { LambdaLlrtStack } from "../src/stacks";
 
 test("Snapshot", () => {
-  const app = new App();
-  const stack = new MyStack(app, "test");
+    const app = new App();
+    const stack = new LambdaLlrtStack(app, "test");
 
-  const template = Template.fromStack(stack);
-  expect(template.toJSON()).toMatchSnapshot();
+    const template = Template.fromStack(stack);
+    expect(template.toJSON()).toMatchSnapshot();
 });
