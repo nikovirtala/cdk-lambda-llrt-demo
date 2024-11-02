43e40a852800129ef95b5e5ddb7132434fce9afa
Niko Virtala <niko.virtala@hey.com>
2024-11-02 15:28:07 +0200

    chore(deps): update cdk to version 2.165.0

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,8 +1,9 @@
 import { AwsCdkApp } from "@nikovirtala/projen-aws-cdk-app";
+
 const project = new AwsCdkApp({
-  cdkVersion: "2.1.0",
+  cdkVersion: "2.165.0",
   defaultReleaseBranch: "main",
-  devDeps: ["@nikovirtala/projen-aws-cdk-app@0.0.11"],
+  devDeps: ["@nikovirtala/projen-aws-cdk-app"],
   name: "cdk-lambda-llrt-demo",
   projenrcTs: true,
 
@@ -10,4 +11,5 @@ const project = new AwsCdkApp({
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
 project.synth();
